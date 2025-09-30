6d4e81725c411d5abfb7d6dd7c24f27f5fc26da6
Kopano-Damane-BYUI <kdamane@byupathway.edu>
2025-10-01 00:26:55 +0200

    Finished W04 Learning Activities: Inserting and Validating Data

---
+++ routes/accountRoute.js
@@ -0,0 +1,26 @@
+/* ***********************
+ * Account Routes
+ *************************/
+const express = require("express");
+const router = new express.Router();
+const utilities = require("../utilities");
+const accountController = require("../controllers/accountController");
+const regValidate = require("../utilities/account-validation");
+
+
+// Deliver login view
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+
+// Deliver registration view
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registrationRules(), // apply validation rules
+  regValidate.checkRegData,        // run the checks
+  utilities.handleErrors(accountController.registerAccount) // proceed if no errors
+);
+
+
+module.exports = router;
